fc30b3c6244eb7628c4eab7c7618d80c1eb231a7
Jose_Fernandez <jfernand84@gmail.com>
2021-10-14 14:46:32 -0400

    need to build db

--- server/app.js
+++ server/app.js
@@ -12,9 +12,17 @@ app.use(morgan('dev'));
 //parsing middleware
 app.use(express.json());
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+// );
+
+app.get('/', (req, res) => {
+  try {
+    res.send('Jose');
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //static file-serving middleware
 app.use(express.static(path.join(__dirname, '..', 'public')));
