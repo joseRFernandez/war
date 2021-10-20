2d72138c9c1d1e5727e26baa462003d785187da4
Jose_Fernandez <jfernand84@gmail.com>
2021-10-20 08:22:53 -0400

    logic almost wrapped up - need to send data to db

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -22,22 +22,50 @@ export default function GameBoard(props) {
     setCard2(deck2[0]);
   }, [deck2]);
 
-  function compareCards(data) {
+  function compareCards() {
     //build initial pile and add both cards
-    let pile =  [];
+    let pile = [];
     pile.push(card1, card2);
 
-    if (card1.value === 'A') {
-      card1.value = '14';
+    if (card1.value === 'J') {
+      card1.value = "11";
+    }
+    if (card2.value === 'J') {
+      card2.value = "11";
+    }
+    if (card1.value === 'Q') {
+      card1.value = "12";
+    }
+    if (card2.value === 'Q') {
+      card2.value = "12";
+    }
+    if (card1.value === 'K') {
+      card1.value = "13";
+    }
+    if (card2.value === 'K') {
+      card2.value = "13";
     }
     if (card2.value === 'A') {
-      card2.value = '14';
+      card2.value = "14";
+    }
+    if (card1.value === 'A') {
+      card1.value = "14";
     }
 
+    card1.value = parseInt(card1.value);
+    card2.value = parseInt(card2.value);
+
+    console.log(card1, ' -- ', card2, '--', deck1);
     if (card1.value === card2.value) {
-      const builtPile =iDeclareWar(pile);//returns here
+      const builtPile = iDeclareWar(pile); //returns here
       const payload = builtPile;
-      console.log('payload after ideclarewar: ', payload)
+      console.log('payload after ideclarewar: ', payload);
+      if (payload.data.card1.value > payload.data.card2.value) {
+        postDeclareWarPlayer1Victory(payload);
+      }
+      if (payload.data.card2.value > payload.data.card1.value) {
+        postDeclareWarPlayer2Victory(payload);
+      }
     }
 
     //player 1 wins 1 card battle
@@ -52,31 +80,62 @@ export default function GameBoard(props) {
       setDeck2(tempDeck2);
     }
     //player2 wins 1 card battle
-    if (card2.value > card1.value) {
+    if (card2.value>  card1.value) {
       //create new temp decks
       const tempDeck1 = deck1.slice(1);
       const tempDeck2 = deck2.slice(1);
       tempDeck2.push(...pile);
-      setDeck2(tempDeck2.flat());
+      setDeck2(tempDeck2);
       //handle win
       setDeck1(tempDeck1);
       setDeck2(tempDeck2);
     }
   }
 
-  const iDeclareWar = (pile) => {
-    alert('TIE')
-
-    const cards1= [...deck1].slice(0,4)
-    const cards2= [...deck2].slice(0, 4);
-
-    const newCard1 = cards1[cards1.length - 1]
-    const newCard2 = cards2[cards2.length - 1]
-
-    pile = [...cards1 , ...cards2].flat();
-
-   console.log(({data: {card1: newCard1, card2: newCard2, pile}}));
+  const postDeclareWarPlayer1Victory = (data) => {
+    const { pile } = data.data;
+    console.log(pile);
+    const tempDeck1 = deck1.slice(pile.length / 2);
+    tempDeck1.push(...pile);
+    setDeck1(tempDeck1);
+    const tempDeck2 = deck2.slice(pile.length / 2);
+    //handle win
+    setDeck1(tempDeck1);
+    setDeck2(tempDeck2);
   };
+
+  const postDeclareWarPlayer2Victory = (data) => {
+    const { pile } = data.data;
+    const tempDeck2 = deck2.slice(pile.length / 2);
+    tempDeck2.push(...pile);
+    setDeck2(tempDeck2);
+    const tempDeck1 = deck1.slice(pile.length / 2);
+    //handle win
+    setDeck1(tempDeck1);
+    setDeck2(tempDeck2);
+  };
+
+  const iDeclareWar = (pile) => {
+    alert('TIE');
+
+    const cards1 = [...deck1].slice(0, 4);
+    const cards2 = [...deck2].slice(0, 4);
+
+    const newCard1 = cards1[cards1.length - 1];
+    const newCard2 = cards2[cards2.length - 1];
+
+    pile = [...cards1, ...cards2].flat();
+
+    return { data: { card1: newCard1, card2: newCard2, pile } };
+  };
+
+
+  if (deck1.length < 1) {
+    return <h1>Player 2 Wins</h1>
+  }
+  if (deck2.length < 1) {
+    return <h2>Player 2 Wins</h2>
+  }
 
   if (card1 && card2) {
     return (
